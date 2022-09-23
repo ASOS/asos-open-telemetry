cce6dbef5f7222358c8a6b73132b0a819c9241cc
Dylan Morley <dylanm@asos.com>
2022-09-23 13:30:52 +0100

    Better public API documentation

--- Asos.OpenTelemetry.Exporter.EventHubs/Asos.OpenTelemetry.Exporter.EventHubs/AuthenticationMode.cs
+++ Asos.OpenTelemetry.Exporter.EventHubs/Asos.OpenTelemetry.Exporter.EventHubs/AuthenticationMode.cs
@@ -1,7 +1,17 @@
 namespace Asos.OpenTelemetry.Exporter.EventHubs;
 
+/// <summary>
+/// Authentication modes supported when exporting data to Event Hub
+/// </summary>
 public enum AuthenticationMode
 {
+    /// <summary>
+    /// SasKey format. Generate a shared access signature, using a key name and access key
+    /// </summary>
     SasKey = 0,
+    /// <summary>
+    /// Managed Identity. Will used DefaultAzureCredential to acquire an access token scoped for
+    /// event hubs resources and send as Bearer token
+    /// </summary>
     ManagedIdentity = 1
 }
--- Asos.OpenTelemetry.Exporter.EventHubs/Asos.OpenTelemetry.Exporter.EventHubs/MeterProviderExtensions.cs
+++ Asos.OpenTelemetry.Exporter.EventHubs/Asos.OpenTelemetry.Exporter.EventHubs/MeterProviderExtensions.cs
@@ -4,8 +4,19 @@ using OpenTelemetry.Metrics;
 
 namespace Asos.OpenTelemetry.Exporter.EventHubs;
 
+/// <summary>
+/// Extensions class for adding the exporter to the Open Telemetry configuration
+/// </summary>
 public static class MeterProviderExtensions
 {
+    /// <summary>
+    /// Adds <see cref="OtlpMetricExporter"/> to the <see cref="MeterProviderBuilder"/> using
+    /// the <see cref="EventHubOptions"/> specified. Configures exporting OTLP to Event Hubs
+    /// using Protobuf
+    /// </summary>
+    /// <param name="builder">The instance of <see cref="MeterProviderBuilder"/> to chain the calls</param>
+    /// <param name="options">The instance of <see cref="EventHubOptions"/> used to configure the Event Hub target</param>
+    /// <returns>The instance of <see cref="MeterProviderBuilder"/></returns>
     public static MeterProviderBuilder AddOtlpEventHubExporter(
         this MeterProviderBuilder builder, EventHubOptions options)
     {
